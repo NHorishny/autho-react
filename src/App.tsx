1fde5c89a3848ee04de380c504a30cd02b583523
NHorishny <Nich.Horishny@simplyhealth.co.uk>
2024-10-07 14:48:11 +0100

    IsLoading and Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import AuthComponent from './components/AuthComponent';
 
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center" >
       <AuthComponent />
         
       </header>
--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import LogoutButton from "./Authentication/Logout";
 
 /**
  * A React component that manages authentication with Auth0.
@@ -7,72 +8,46 @@ import { useEffect,useState } from "react";
  * @returns {JSX.Element} A JSX element that displays the user's name and a logout button if they are authenticated.
  */
 function AuthComponent(): JSX.Element {
-    const { loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const { loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently, isLoading } = useAuth0();
     const [token, setToken] = useState<string | null>(null);
-
-    const handleLogin = async () => {
-        await loginWithRedirect({
-            appState: { targetUrl: window.location.pathname },
-            //@ts-ignore 
-            redirectUri: window.location.origin,
-          });
-        //   callApi();
-        };
     
-    //   const callApi = async () => {
-    //     const token = await getAccessTokenSilently();
-    //     const response = await fetch('https://authotest.uk.auth0.com/api/v2/', {
-    //       headers: {
-    //         Authorization: `Bearer ${token}`,
-    //       },
-    //       credentials: 'include',
-    //     });
-    
-    //     if (!response.ok) {
-    //       console.error('API call failed:', response);
-    //     } else {
-    //       const data = await response.json();
-    //       console.log('API response:', data);
-    //     }
-    //   };
-    
-    /**
-     * Fetches an access token silently if the user is authenticated.
-     */
-    useEffect(() => {
-        const fetchToken = async () => {
-          if (isAuthenticated) {
-            const token = await getAccessTokenSilently({
-            //@ts-ignore 
-              audience: 'https://authotest.uk.auth0.com/userinfo',
-              scope: 'read:messages write:messages',
-            });
+    const fetchToken = async () => {
+        try {
+            const token = await getAccessTokenSilently();
             console.log('Access Token:', token); // Use the token as needed
             setToken(token);
-            
-            
+        } catch (error) {
+            console.error('Error fetching access token:', error);
+        }
+    };
 
-          }
-        
-        };
-    
-        fetchToken();
-      }, [isAuthenticated, getAccessTokenSilently]);
-  
+    useEffect(() => {
+        if (isAuthenticated) {
+            fetchToken();
+        } else if (!token && !isLoading) {
+            loginWithRedirect();
+        }
+    }, [isAuthenticated, isLoading, loginWithRedirect]);
+
+    if (isLoading) {
+        return <div>Loading...</div>; // Display a loading state while authentication status is being determined
+    }
+
     return (
-        <div>
-        {/* @ts-ignore */}
-      {!isAuthenticated && <button onClick={handleLogin}>Log in</button>}
-      {isAuthenticated && (
-        <>
-        {/* @ts-ignore */}
-        <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
-          <h2>Welcome, {user!.name}</h2>
-          <p>Your JWT Token: {token}</p>
-        </>
-      )}
-    </div>
-  );
+      <div className="header-container">
+            {isAuthenticated && (
+                <>
+                    <span className="nav-main">       
+                        <h1 className="authO">AuthOTest</h1>
+                        <a href="https://localhost:7030" className="nav-link">.NET</a>
+                    </span>
+                    <div className="flex-container">
+                        <h2 className="nav-link">Welcome, {user?.name}</h2>
+                        <LogoutButton />
+                    </div>
+                </>
+            )}
+        </div>
+    );
 }
-
 export default AuthComponent;
